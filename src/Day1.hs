565824965ab01449d69158dd5f0fc8584cef7542
Tom Gleghorn <tgleghorn@covermymeds.com>
2019-12-03 10:03:12 -0500

    day1 problem2 solution

--- src/Day1.hs
+++ src/Day1.hs
@@ -5,13 +5,24 @@ module Day1
 printTotalFuelRequired :: String -> IO ()
 printTotalFuelRequired inputFile = do
   file <- readFile inputFile
-  let linesOfFile = lines file
-  let masses = [parseMass x | x <- linesOfFile]
-  let fuel = sum [calculateFuel x | x <- masses]
-  putStrLn ("Total fuel requred for all modules: " ++ (show fuel))
+  let masses = [parseMass x | x <- (lines file)]
+  let fuel = sum [naiveCalculateFuel x | x <- masses]
+  let actualFuel = sum [calculateFuel x | x <- masses]
+  putStrLn ("D01P1: Naive total fuel requred for all modules: " ++ (show fuel))
+  putStrLn ("D02P2: Actual total fuel requred for all modules: " ++ (show actualFuel))
 
 parseMass :: String -> Integer
 parseMass massString = read massString :: Integer
 
+naiveCalculateFuel :: Integer -> Integer
+naiveCalculateFuel mass = floor (fromIntegral (mass)/3) - 2
+
 calculateFuel :: Integer -> Integer
-calculateFuel mass = floor (fromIntegral (mass)/3) - 2
+calculateFuel mass = 
+  if
+    fuel > 0
+  then
+    fuel + (calculateFuel fuel)
+  else
+    0
+  where fuel = naiveCalculateFuel mass
