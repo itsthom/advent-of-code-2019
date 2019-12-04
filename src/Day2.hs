3ac8f989800c47ef9ec4d1883425f7dbd437427f
Tom Gleghorn <tgleghorn@covermymeds.com>
2019-12-04 15:18:27 -0500

    day2 problem2 solution

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,21 +1,37 @@
 module Day2
-    ( runIntcodeProgram,
-      execute,
-      operate,
-      replaceAt,
-      toIntCode,
-      splitStr
+    ( runIntcodeProgram
     ) where
 
+import qualified Data.List as L
 import qualified Data.Text as T
 
 runIntcodeProgram :: String -> IO ()
 runIntcodeProgram inputFile = do
   file <- readFile inputFile
   let intCode = toIntCode file
-  let intCode1202 = replaceAt (replaceAt intCode 2 2) 1 12
-  let result = execute intCode1202 0
-  putStrLn ("D02.1 -> IntCode output is: " ++ (show result))
+  let intCode1202 = initialize intCode 12 2
+  let result1202 = execute intCode1202 0
+  let foundInput = findInput intCode 19690720
+  putStrLn ("D02.1 -> IntCode output is: " ++ (show (head result1202)))
+  putStrLn ("D02.2 -> Required input is: " ++ (show foundInput))
+
+findInput :: [Int] -> Int -> Int
+findInput code desiredOutput =
+  100 * x + y
+  where (x,y) = solution code desiredOutput
+
+solution :: [Int] -> Int -> (Int,Int)
+solution code desiredOutput =
+  case foundSolution of
+    Just foundSolution -> foundSolution
+    Nothing -> (999999,999999)
+  where problemSpace = [(x,y) | x <- [0..99], y <- [0..99]]
+        foundSolution = L.find (testSolution code desiredOutput) problemSpace
+
+testSolution :: [Int] -> Int -> (Int,Int) -> Bool
+testSolution code desiredOutput input =
+  head (execute initializedCode 0) == desiredOutput
+  where initializedCode = initialize code (fst input) (snd input)
 
 execute :: [Int] -> Int -> [Int]
 execute code index
@@ -33,6 +49,9 @@ operate operator code index =
         num1 = code!!(code!!(index + 1))
         num2 = code!!(code!!(index + 2))
         nextIndex = index + 4
+
+initialize :: [Int] -> Int -> Int -> [Int]
+initialize code noun verb = replaceAt (replaceAt code 2 verb) 1 noun
 
 replaceAt :: [Int] -> Int -> Int -> [Int]
 replaceAt source index newValue =
