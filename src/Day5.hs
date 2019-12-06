888f72e0baf22f7b088d56ea750b83727f807726
Tom Gleghorn <tgleghorn@covermymeds.com>
2019-12-05 22:01:30 -0500

    start redesigning interpreter

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,5 +1,8 @@
 module Day5
-    ( runTESTIntcodeProgram
+    ( runTESTIntcodeProgram,
+      execute,
+      toOpcode,
+      toParameterMode
     ) where
 
 import qualified Data.List as L
@@ -8,30 +11,48 @@ import Utils
 runTESTIntcodeProgram :: String -> IO ()
 runTESTIntcodeProgram inputFile = do
   file <- readFile inputFile
-  let intCode = toIntCode file
-  let intCode1202 = initialize intCode 12 2
-  let result1202 = head (execute intCode1202 0)
-  putStrLn ("D05.1 -> IntCode output is: " ++ (show result1202))
+  let intcode = toIntCode file
+  let result = execute intcode 0
+  putStrLn ("D05.1 -> IntCode output is: " ++ "TBD")
 
-execute :: [Int] -> Int -> [Int]
-execute code index
-  | opCode == 1 = operate (+) code index
-  | opCode == 2 = operate (*) code index
-  | opCode == 99 = code
-  | otherwise = execute code (index + 1)
-  where opCode = code!!index
+type Intcode = [Int]
+data Opcode = Add | Multiply | Write | Read | END deriving (Enum, Eq, Show)
+data ParamMode = Position | Immediate deriving (Enum, Eq, Show)
+data Instruction = Instruction { index :: Int
+                               , opCode :: Maybe Opcode
+                               , firstParam :: (Int, ParamMode)
+                               , secondParam :: (Int, ParamMode)
+                               , thirdParam :: (Int, ParamMode)
+                               } deriving (Show)
 
-operate :: (Int -> Int -> Int) -> [Int] -> Int -> [Int]
-operate operator code index = 
-  execute result nextIndex
-  where result = Utils.replaceAt code destination (operator num1 num2)
-        num1 = code!!(code!!(index + 1))
-        num2 = code!!(code!!(index + 2))
-        destination = code!!(index + 3)
-        nextIndex = index + 4
+execute :: Intcode -> Int -> Intcode
+execute code index = case opCode instruction of
+  Just Add -> [1] -- TODO performAddition, etc.
+  Just Multiply -> [1]
+  Just Read -> [1]
+  Just Write -> [1]
+  Just END -> [1]
+  Nothing -> [999999] -- something has gone horribly wrong
+  where instruction = parseInstruction code index
 
-initialize :: [Int] -> Int -> Int -> [Int]
-initialize code noun verb = Utils.replaceAt (Utils.replaceAt code 2 verb) 1 noun
+parseInstruction :: Intcode -> Int -> Instruction
+parseInstruction code index =
+  Instruction {index = index,opCode = Just END,firstParam = (1,Position),secondParam = (1,Position),thirdParam = (1,Position)}
+
+toOpcode :: Integral a => a -> Maybe Opcode
+toOpcode x = case x of
+  1 -> Just Add
+  2 -> Just Multiply
+  3 -> Just Read
+  4 -> Just Write
+  99 -> Just END
+  otherwise -> Nothing
+
+toParameterMode :: Integral a => a -> Maybe ParamMode
+toParameterMode x = case x of
+  0 -> Just Position
+  1 -> Just Immediate
+  otherwise -> Nothing
 
 toIntCode :: String -> [Int]
 toIntCode input = map read (Utils.splitStr "," input) 
